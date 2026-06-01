7bbf22b7c44f00450021dddc0f2c58605d4f11e4
Sarvesh <sarveshnakhale21@gmail.com>
2026-06-01 11:35:31 +0530

    Update SVG content and ASCII art in index.py

--- api/index.py
+++ api/index.py
@@ -20,31 +20,46 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         stats = get_github_stats("Sarvesh2005-code")
         
-        # Replace the ASCII art below with your generated face
         svg_content = f"""
         <svg width="850" height="420" xmlns="http://www.w3.org/2000/svg">
           <style>
             .text {{ font-family: 'Courier New', Courier, monospace; font-size: 13px; fill: #C9D1D9; }}
+            .ascii {{ font-family: 'Courier New', Courier, monospace; font-size: 11px; fill: #58A6FF; }}
             .blue {{ fill: #58A6FF; }}
             .green {{ fill: #7EE787; }}
-            .orange {{ fill: #D2A8FF; }}
+            .orange {{ fill: #FF9D00; }}
             .grey {{ fill: #8B949E; }}
           </style>
           <rect width="850" height="420" rx="10" fill="#0D1117" stroke="#30363D" stroke-width="1"/>
           
-          <!-- ASCII Face Silhouette (Replace this block) -->
-          <text x="40" y="60" class="text blue" xml:space="preserve">
-         ,,,,, 
-       ,;;;;;;;, 
-      ;;;;;;;;;;; 
-     ;;;;;'''';;;; 
-     ;;;;;    ;;;; 
-     `;;;;,  ,;;;' 
-       `;;;;;;;;' 
-         `''''` 
+          <text x="20" y="40" class="ascii" xml:space="preserve">
+             g@M%@%%@N%Nw,,
+           ,M*|`||*%gNM=]mM%g||%N,
+           p!``  `! |``` ```|||jhlj%w
+         ,@L     ,,       ```!`|j%M]%M
+        ]j`` .,wp@pw,     `    ````|%Wg
+      /{{||]@@@@@@@@@@pp.           |||||
+      ` ' ]@@@@@@@@@@@@@p     ,  ,```
+        :]%@@@@@%%%%%%k%h  '*||mkr    *
+        j%M`      |jkk'  ~nrn=|i    ;`
+      !  jrr*^`           `"! L'':!
+       j  lp;,.  ,/ @@    ,;\\nmy  " ,~
+       i r @@@@mmHM @@@@ `^****M*,p ;,
+       | ]@@@HHH]g@M%%%%%H,jmgpmb%  j
+       ;;%%%%%%k%@[,.n|;.;j%%k|k%%',[
+        H|%%k%%%j%k||,;;j;!!'|%ij}}]@
+        "djjmkL,"]]][,,,,wwxw;|#kjk`
+          %;%km%%%%M%M|%%jkkii|||[
+           kjj%kkkl!|||||||j|||"
+            |jm%H@@@b%%kkmk%i|!,[
+             @p|j%%%%jkk|||j*``;j[
+             ]@@@g|```````   ,,;j%k
+             @@@@@mgmp;,,,,:;jj%%k%
+            @@@@@@@@@%%kgki!|jjjj%k%@ .
+       ^['' %@@@HH%b%k{{illljkjj%%% ; `,.
+      =['` . %HH%%%%%H@gkilljjj%kk%".  `'i
           </text>
 
-          <!-- System & Profile Info -->
           <text x="320" y="50" class="text" xml:space="preserve"><tspan class="blue">Sarvesh</tspan>@<tspan class="blue">Developer</tspan> <tspan class="grey">--------------------------------</tspan></text>
           
           <text x="320" y="80" class="text" xml:space="preserve">. <tspan class="orange">OS:</tspan> .......................... Windows 11, Kali Linux</text>
@@ -57,14 +72,12 @@ class handler(BaseHTTPRequestHandler):
           <text x="320" y="220" class="text" xml:space="preserve">. <tspan class="orange">Hobbies.Software:</tspan> .... Backend Architecture, Spring Boot</text>
           <text x="320" y="240" class="text" xml:space="preserve">. <tspan class="orange">Hobbies.Security:</tspan> ........ NIST Frameworks, Custom Firewalls</text>
 
-          <!-- Contact Info -->
           <text x="320" y="280" class="text" xml:space="preserve"><tspan class="grey">- Contact ------------------------------------------</tspan></text>
           <text x="320" y="310" class="text" xml:space="preserve">. <tspan class="orange">Email:</tspan> ..................... sarveshnakhale21@gmail.com</text>
-          <text x="320" y="330" class="text" xml:space="preserve">. <tspan class="orange">Website:</tspan> ...................... https://sarvy.vercel.app/</text>
+          <text x="320" y="330" class="text" xml:space="preserve">. <tspan class="orange">Website:</tspan> ...................... [https://sarvy.vercel.app/](https://sarvy.vercel.app/)</text>
           <text x="320" y="350" class="text" xml:space="preserve">. <tspan class="orange">LinkedIn:</tspan> ............................. in/sarveshnakhale</text>
           <text x="320" y="370" class="text" xml:space="preserve">. <tspan class="orange">X:</tspan> .......................................... @Sarvyx2005</text>
 
-          <!-- Live GitHub Stats -->
           <text x="320" y="410" class="text" xml:space="preserve"><tspan class="grey">- GitHub Stats [{stats['status']}] ------------------------</tspan></text>
           <text x="320" y="440" class="text" xml:space="preserve">. <tspan class="orange">Repos:</tspan> .... <tspan class="green">{stats['repos']}</tspan> | <tspan class="orange">Followers:</tspan> ....... <tspan class="green">{stats['followers']}</tspan></text>
         </svg>
