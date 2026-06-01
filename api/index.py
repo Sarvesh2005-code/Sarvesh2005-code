9c52586488a4ac17beec3c854004539c31d987b1
Sarvesh <sarveshnakhale21@gmail.com>
2026-06-01 13:03:35 +0530

    Update SVG dimensions and text formatting

--- api/index.py
+++ api/index.py
@@ -20,7 +20,6 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         stats = get_github_stats("Sarvesh2005-code")
         
-        # We split the URLs here so your rich-text editor doesn't accidentally format them!
         w3_namespace = "http://" + "www.w3.org/2000/svg"
         my_website = "https://" + "sarvy.vercel.app/"
         
@@ -53,48 +52,49 @@ class handler(BaseHTTPRequestHandler):
         ]
 
         tspan_blocks = ""
-        y_pos = 40
+        y_pos = 65  
         for line in ascii_art_lines:
             clean_line = line.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
-            tspan_blocks += f'<tspan x="20" y="{y_pos}">{clean_line}</tspan>\n          '
-            y_pos += 14  
+            tspan_blocks += f'<tspan x="30" y="{y_pos}">{clean_line}</tspan>\n          '
+            y_pos += 15  
 
         svg_content = f"""
-        <svg width="850" height="420" xmlns="{w3_namespace}">
+        <svg width="900" height="500" xmlns="{w3_namespace}">
           <style>
-            .text {{ font-family: 'Courier New', Courier, monospace; font-size: 13px; fill: #C9D1D9; }}
-            .ascii {{ font-family: 'Courier New', Courier, monospace; font-size: 11px; fill: #58A6FF; }}
+            .text {{ font-family: 'Courier New', Courier, monospace; font-size: 14px; fill: #C9D1D9; }}
+            .ascii {{ font-family: 'Courier New', Courier, monospace; font-size: 12px; fill: #58A6FF; }}
             .blue {{ fill: #58A6FF; }}
             .green {{ fill: #7EE787; }}
-            .orange {{ fill: #FF9D00; }}
+            .orange {{ fill: #E3B341; }}
             .grey {{ fill: #8B949E; }}
           </style>
-          <rect width="850" height="420" rx="10" fill="#0D1117" stroke="#30363D" stroke-width="1"/>
+          <rect width="900" height="500" rx="10" fill="#161B22" stroke="#30363D" stroke-width="1"/>
           
           <text class="ascii" xml:space="preserve">
           {tspan_blocks}
           </text>
 
-          <text x="320" y="50" class="text" xml:space="preserve"><tspan class="blue">Sarvesh</tspan>@<tspan class="blue">Developer</tspan> <tspan class="grey">--------------------------------</tspan></text>
+          <text x="350" y="65" class="text" xml:space="preserve"><tspan class="blue">Sarvesh</tspan>@<tspan class="blue">Developer</tspan> <tspan class="grey">----------------------------------</tspan></text>
           
-          <text x="320" y="80" class="text" xml:space="preserve">. <tspan class="orange">OS:</tspan> .......................... Windows 11, Kali Linux</text>
-          <text x="320" y="100" class="text" xml:space="preserve">. <tspan class="orange">Uptime:</tspan> .................................... 20 years</text>
-          <text x="320" y="120" class="text" xml:space="preserve">. <tspan class="orange">IDE:</tspan> ............................... Eclipse, VS Code</text>
+          <text x="350" y="100" class="text" xml:space="preserve">. <tspan class="orange">OS:</tspan> ........................... Windows 11, Kali Linux</text>
+          <text x="350" y="120" class="text" xml:space="preserve">. <tspan class="orange">Uptime:</tspan> ..................................... 20 years</text>
+          <text x="350" y="140" class="text" xml:space="preserve">. <tspan class="orange">IDE:</tspan> ................................ Eclipse, VS Code</text>
           
-          <text x="320" y="160" class="text" xml:space="preserve">. <tspan class="orange">Languages.Programming:</tspan> ......................... Java</text>
-          <text x="320" y="180" class="text" xml:space="preserve">. <tspan class="orange">Languages.Computer:</tspan> ....... HTML, CSS, JS, SQL, JSON</text>
+          <text x="350" y="180" class="text" xml:space="preserve">. <tspan class="orange">Languages.Programming:</tspan> .......................... Java</text>
+          <text x="350" y="200" class="text" xml:space="preserve">. <tspan class="orange">Languages.Computer:</tspan> ........ HTML, CSS, JS, SQL, JSON</text>
           
-          <text x="320" y="220" class="text" xml:space="preserve">. <tspan class="orange">Hobbies.Software:</tspan> .... Backend Architecture, Spring Boot</text>
-          <text x="320" y="240" class="text" xml:space="preserve">. <tspan class="orange">Hobbies.Security:</tspan> ........ NIST Frameworks, Custom Firewalls</text>
+          <text x="350" y="240" class="text" xml:space="preserve">. <tspan class="orange">Hobbies.Software:</tspan> ..... Backend Architecture, Spring Boot</text>
+          <text x="350" y="260" class="text" xml:space="preserve">. <tspan class="orange">Hobbies.Security:</tspan> ......... NIST Frameworks, Custom Firewalls</text>
 
-          <text x="320" y="280" class="text" xml:space="preserve"><tspan class="grey">- Contact ------------------------------------------</tspan></text>
-          <text x="320" y="310" class="text" xml:space="preserve">. <tspan class="orange">Email:</tspan> ..................... sarveshnakhale21@gmail.com</text>
-          <text x="320" y="330" class="text" xml:space="preserve">. <tspan class="orange">Website:</tspan> ...................... {my_website}</text>
-          <text x="320" y="350" class="text" xml:space="preserve">. <tspan class="orange">LinkedIn:</tspan> ............................. in/sarveshnakhale</text>
-          <text x="320" y="370" class="text" xml:space="preserve">. <tspan class="orange">X:</tspan> .......................................... @Sarvyx2005</text>
+          <text x="350" y="305" class="text" xml:space="preserve"><tspan class="grey">- Contact --------------------------------------------</tspan></text>
+          <text x="350" y="335" class="text" xml:space="preserve">. <tspan class="orange">Email:</tspan> ...................... sarveshnakhale21@gmail.com</text>
+          <text x="350" y="355" class="text" xml:space="preserve">. <tspan class="orange">Website:</tspan> ....................... {my_website}</text>
+          <text x="350" y="375" class="text" xml:space="preserve">. <tspan class="orange">LinkedIn:</tspan> .............................. in/sarveshnakhale</text>
+          <text x="350" y="395" class="text" xml:space="preserve">. <tspan class="orange">X:</tspan> ........................................... @Sarvyx2005</text>
+          <text x="350" y="415" class="text" xml:space="preserve">. <tspan class="orange">Discord:</tspan> ....................................... @sarvy123</text>
 
-          <text x="320" y="410" class="text" xml:space="preserve"><tspan class="grey">- GitHub Stats [{stats['status']}] ------------------------</tspan></text>
-          <text x="320" y="440" class="text" xml:space="preserve">. <tspan class="orange">Repos:</tspan> .... <tspan class="green">{stats['repos']}</tspan> | <tspan class="orange">Followers:</tspan> ....... <tspan class="green">{stats['followers']}</tspan></text>
+          <text x="350" y="460" class="text" xml:space="preserve"><tspan class="grey">- GitHub Stats [{stats['status']}] --------------------------</tspan></text>
+          <text x="350" y="485" class="text" xml:space="preserve">. <tspan class="orange">Repos:</tspan> .... <tspan class="green">{stats['repos']}</tspan> | <tspan class="orange">Followers:</tspan> ....... <tspan class="green">{stats['followers']}</tspan></text>
         </svg>
         """
 
